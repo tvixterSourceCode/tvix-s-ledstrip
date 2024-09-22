e8b9488073fd4cca057de669ca346461ab7d43fb
tvixterSourceCode <filip201070@gmail.com>
2024-09-22 14:56:22 +0200

    Added wave mode

--- tvix's led strip/src/main.cpp
+++ tvix's led strip/src/main.cpp
@@ -236,7 +236,35 @@ void loop() {
       setted = true;
     }
     break;
-  
+  case 'w':
+    if (millis() - eventlasttime >= .01) 
+    {
+      //Serial.print(num_passed);
+      //num_passed = constrain((num_passed + 1) % num-1,0,num);
+      num_passed += 1;
+      num_passed = (num_passed) % num;
+      eventlasttime = millis();
+
+      change_led(CRGB(0,0,0),max_bright,0,constrain(num_passed - farness,0,num - farness),false);
+
+      for (int i = num_passed; i >= constrain(num_passed - farness,0,num); i--) 
+      {
+        //Serial.println(red * (1-(float)abs(i - num_passed) / farness));
+        change_led(CRGB((red * (1-(float)abs(i - num_passed) / farness))* mutliplier_of_w,(green * (1-(float)abs(i - num_passed) / farness))* mutliplier_of_w,(blue * (1-(float)abs(i - num_passed) / farness))* mutliplier_of_w),max_bright,i-1, i, false);
+      }
+
+
+      for (int i = num_passed+1; i <= constrain(num_passed + farness,0,num); i++)
+      {
+        //Serial.println(red * (1-(float)abs(i - num_passed) / farness));
+        Serial.println(num_passed);
+        change_led(CRGB((red * (1-(float)abs(i - num_passed) / farness))* mutliplier_of_w,(green * (1-(float)abs(i - num_passed) / farness))* mutliplier_of_w,(blue * (1-(float)abs(i - num_passed) / farness))* mutliplier_of_w),max_bright,i,i+1,false);
+      }
+      change_led(CRGB(red,green,blue),max_bright,num_passed,num_passed + 1,false); //center
+
+      change_led(CRGB(0,0,0),max_bright,constrain(num_passed + farness,10,90),num,false);
+    }
+    break;
   }
   if (millis() - unbuttonlasttime >= 3000)
   {
